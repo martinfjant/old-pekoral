c49fb7aa962e347832927d36289f02d70d6f929b
martinfjant <martin@falkjohansson.se>
2019-11-17 21:25:37 +0100

    added toggle for search

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
-import list from './modules/list'
+import list from './modules/list';
 
-console.log(`Hello ${list[0]}`)
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+hamburger.addEventListener('click', toggle_menu);
+
+function toggle_menu() {
+  hamburger.classList.toggle('hamburger_open');
+  menu.classList.toggle('hidden');
+}
+
+const lookingglass = document.getElementById('lookingglass');
+const search = document.getElementById('search');
+lookingglass.addEventListener('click', toggle_search);
+
+function toggle_search() {
+  search.classList.toggle('hidden');
+}
