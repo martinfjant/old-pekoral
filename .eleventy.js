6e19e4e1d74c0ad328abc95bb2babb521cb8571c
martinfjant <martin@falkjohansson.se>
2019-12-08 12:34:58 +0100

    added enuma elish

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addFilter("squash", require("./src/_filters/squash.js"));
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: "src", output: "dist", data: "_data" },
     passthroughFileCopy: true,
-    templateFormats: ['pug', 'njk', 'md', 'css', 'html', 'yml'],
-    htmlTemplateEngine: 'njk',
+    templateFormats: ["pug", "njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk"
   };
 };
